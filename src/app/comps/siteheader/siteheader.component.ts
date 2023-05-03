31c20d490fedd04faac05914601141399cc1c996
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-03 12:49:49 +0530

    header design complete

---
+++ src/app/comps/siteheader/siteheader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-siteheader',
+  templateUrl: './siteheader.component.html',
+  styleUrls: ['./siteheader.component.css']
+})
+export class SiteheaderComponent {
+
+}
