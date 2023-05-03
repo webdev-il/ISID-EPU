087cce731eff9365360e7b5da0b6e0bedb98f5b9
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-03 13:28:55 +0530

    deploy commit message fix.

--- deploy.ps1
+++ deploy.ps1
@@ -5,6 +5,7 @@ bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.js index.html'
 ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
 bash -c 'mv dist/isid-epu/* .';
 git add -A;
-git commit -m "deployed.";
+$dt = Get-Date
+git commit -m "$dt";
 git push origin deploy --force;
 git checkout master;
