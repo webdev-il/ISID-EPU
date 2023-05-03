02a5e16110a8a62440d38bfde201f2db7e1fa8fd
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-03 13:31:19 +0530

    deploy script fixes.

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,11 @@
-python deploy.py;
+bash -c 'rm -rf .angular node_modules/.cache dist'
+git checkout deploy
+git merge master
+bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
 ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
 bash -c 'mv dist/isid-epu/* .';
 git add -A;
-git commit -m "deployed.";
+$dt = Get-Date
+git commit -m "$dt";
 git push origin deploy --force;
 git checkout master;
