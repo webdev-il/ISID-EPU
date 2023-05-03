bf61805b7a2ed3be97cf9c710e6a5bd88aed2696
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-03 13:26:25 +0530

    tracking all deploys.

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,7 @@
-python deploy.py;
+bash -c 'rm -rf .angular node_modules/.cache dist'
+git checkout deploy
+git merge master
+bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.js index.html'    
 ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
 bash -c 'mv dist/isid-epu/* .';
 git add -A;
