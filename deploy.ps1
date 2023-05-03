83f2c3a43931b1b15b28cabeab0380d11e21a0bf
Hardik <hardikrajpal@cse.iitb.ac.in>
2023-05-03 15:47:27 +0530

    05/03/2023 15:47:27

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,8 @@
-bash -c 'rm -rf .angular node_modules/.cache dist'
-git checkout deploy
-git merge master
-bash -c 'rm main.*.js polyfills.*.js runtime.*.js styles.*.css index.html'    
+    
 ng build --base-href https://hardik-rajpal.github.io/ISID-EPU/;
 bash -c 'mv dist/isid-epu/* .';
 git add -A;
 $dt = Get-Date
 git commit -m "$dt";
 git push origin deploy --force;
-git checkout master;
+# git checkout master;
